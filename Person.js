fd367c6aca636adeaa8fa483a7e279cd12be57d3
keithchasen <keithchasen89@gmail.com>
2024-09-19 21:59:11 +0200

    person animation

--- Person.js
+++ Person.js
@@ -16,6 +16,7 @@ class Person extends GameObject {
 
     update(state) {
         this.updatePosition();
+        this.updateSprite(state);
 
         if (this.isPlayerControlled && this.movingProgressRemaining === 0 && state.arrow) {
             this.direction = state.arrow;
@@ -30,6 +31,18 @@ class Person extends GameObject {
             //this.y            -1
             this[property] += change;
             this.movingProgressRemaining -= 1;
+        }
+    }
+
+    updateSprite(state) {
+        if (this.isPlayerControlled && this.movingProgressRemaining === 0 && !state.arrow) {
+            this.sprite.setAnimation('idle-'+this.direction); // by default idle
+            return;
+        }
+        
+        // if there is still remaining value to walk
+        if (this.movingProgressRemaining > 0) {
+            this.sprite.setAnimation('walk-'+this.direction);
         }
     }
 }
